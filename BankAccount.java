dce960e3707288814e504420c0ffa30a7cbb04d8
J1umpy <limingyou5@gmail.com>
2026-09-10 21:48:45 -0400

    Every successful withdraw will result in a 2 dollar fee

--- BankAccount.java
+++ BankAccount.java
@@ -12,11 +12,12 @@ public class BankAccount {
     }
 
     public boolean withdraw(double amount) {
-        // TODO:
-        // Withdraw the requested amount if sufficient funds are available.
-        // Return true if the withdrawal succeeds.
-        // Return false otherwise.
-        return false;
+        if (balance >= amount + 2){
+            balance -= (amount + 2);
+            return true;
+        }else{
+            return false;
+        }
     }
 
     public String getAccountSummary() {
